76bf2e5676ec76194d16eec1162fc99dc9d7b5b3
mariliapduarte <mariliapduarte@gmail.com>
2022-04-19 17:01:38 -0300

    programação do formulario
    Formspree

--- simplicity-inicial-marilia/js/scripts.js
+++ simplicity-inicial-marilia/js/scripts.js
@@ -83,3 +83,36 @@ textMensagem.addEventListener("input", function (){
     }
 });
 
+//Programação de envio do formulário copiada do Formspree:
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Mensagem enviada. Em breve retornaremos o contato!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! Deu ruim. Tente novamente em alguns minutos"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! Deu ruim. Tente novamente em alguns minutos"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
